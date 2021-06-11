5bc8d501bbb89333e9cb9e593d78c1a028899734
jackcornblum <jcornblum@gmail.com>
2021-06-11 16:46:17 -0400

    added multipled filters

--- src/components/PigPen.js
+++ src/components/PigPen.js
@@ -6,10 +6,20 @@ function PigPen({hogs}){
 
     const [displayedList, setDisplayedList] = useState(hogs)
     const [isFiltered, setIsFiltered] = useState(false)
+    const [weightFilter, setWeightFilter] = useState(false)
 
     let originalHogs = [...displayedList]
 
+    
     let newPigList = originalHogs.filter(pig => pig.greased === true)
+
+    let copyOfNewPigList = [...newPigList]
+
+    let doubleFilterList = copyOfNewPigList.sort((currentHog, nextHog) => {
+        if (currentHog.weight < nextHog.weight) return 1;
+      if (currentHog.weight > nextHog.weight) return -1;
+      return 0;
+     } )
 
     let weightSortedList = originalHogs.sort((currentHog, nextHog) => {
         if (currentHog.weight < nextHog.weight) return 1;
@@ -21,31 +31,45 @@ function PigPen({hogs}){
 
     function handleFilterClick() {
 
-
         setIsFiltered((isFiltered) => !isFiltered)
-      
 
-        
         // setDisplayedList(displayedList)
+    }
+
+    function handleWeightFilter(){
+        setWeightFilter(!weightFilter)
     }
 
     function pigs () {
 
-
-        if (isFiltered === true) {
+        //if grease filter is on AND weight is off
+        if (isFiltered === true && weightFilter === false) {
             return newPigList.map(hog => {
                 return <Pig name={hog.name} image={hog.image} data={hog} />})
-        } else {
+            
+        } 
+        //if grease filter is off AND weight filter is on
+        else if(isFiltered === false && weightFilter === true){
+            return weightSortedList.map(hog => {
+                return <Pig name={hog.name} image={hog.image} data={hog} />})
+        } 
+        //if BOTH filters are on
+        else if(isFiltered === true && weightFilter === true) {
+            return doubleFilterList.map(hog => {
+                return <Pig name={hog.name} image={hog.image} data={hog} />})
+        }
+        //if no filters are on
+         else {
             return displayedList.map(hog => {
                 return <Pig name={hog.name} image={hog.image} data={hog} />})
             }
-        }
+    }
         
     
 
- function handleWeightSorting () {
-     setDisplayedList(weightSortedList)
- }
+//  function handleWeightSorting () {
+//      setDisplayedList(weightSortedList)
+//  }
 
     // console.log(hogs)
     // const pigs = displayedList.map(hog => {
@@ -55,8 +79,8 @@ function PigPen({hogs}){
     return (
 
         <div classname="ui grid container">
-            <button onClick={handleFilterClick}> Filter for Greased Piggies</button> <br></br>
-            <button onClick={handleWeightSorting}> Sort by Weight</button>
+            <button className="ui button" onClick={handleFilterClick}>{isFiltered ? "Back to normal" : "Filter for Greased Piggies"} </button> <br></br>
+            <button className="ui button" onClick={handleWeightFilter}> Sort by Weight</button>
             {pigs()}
         </div>
 
